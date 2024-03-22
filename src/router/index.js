f3c802456a01dcd5a948a5021f7f0ea51c3624fb
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-23 04:00:26 +0800

    Progress: Update Router

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/mainpage',
+      path: '/',
       name: 'mainpage',
       component: () => import('../views/MainPage.vue'),
     },
